5d5abb6e3b995159435e447f265b7c3718dbb1e5
kenchirchir <k.chirchir@alustudent.com>
2026-05-16 15:37:20 +0200

    add MyComponent

---
+++ my-first-react-app/src/MyComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface MyComponentProps {
+  title: string;
+}
+
+const MyComponent: React.FC<MyComponentProps> = ({ title }) => {
+  return (
+    <div className="my-component">
+      <h1>{title}</h1>
+      <p>This is a paragraph of text within my component.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
